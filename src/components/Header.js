518f68d5e46ec90b39c0097eaa9ca577325a0ad5
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-17 03:15:51 +0530

    Added styling to header and also used 'useHistory' hook to go back to the homepage when clicked on the logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import Appbar from "@material-ui/core/AppBar";
+import {
+  Container,
+  MenuItem,
+  Toolbar,
+  Typography,
+  Select,
+  makeStyles,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
+
+
+const useStyle = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
 
 const Header = () => {
+  const classes = useStyle();
+  const history = useHistory();
+
   return (
-    <div>Header  - Theplayer_9</div>
-  )
-}
+    <Appbar position="static" color="primary">
+      <Container>
+        <Toolbar>
+          <Typography
+            onClick={() => history.push("/")}
+            className={classes.title}
+          >
+            Crypto Cosmos{" "}
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              height: 40,
+              marginLeft: 10,
+              backgroundColor: "white",
+            }}
+          >
+            <MenuItem value="USD">USD</MenuItem>
+            <MenuItem value="INR">INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </Appbar>
+  );
+};
 
 export default Header;
