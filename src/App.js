68d5be6dde22131d82b601a378f923604ab233af
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-17 00:07:46 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import Header from './components/Header'
-
+import Header from "./components/Header";
+import CoinPage from "./Pages/CoinPage";
+import HomePage from "./Pages/Homepage";
 
 
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div> <Header /></div>
+      <div>
+        <Header />
+        <Route path="/" component={HomePage} />
+        <Route path="/" component={CoinPage} />
+      </div>
     </BrowserRouter>
   );
 };
