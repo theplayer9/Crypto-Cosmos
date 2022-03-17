f2c60d61634a254fc5e3a4777c508de0ca9854d7
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-18 00:45:58 +0530

    using useContext to make the currency symbol global

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
-import ReactDOM  from "react-dom";
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom";
+import App from "./App";
+import CryptoContext from "./CryptoContext";
+import "./index.css";
 
-ReactDOM.render(<App />,document.getElementById("root"))
+ReactDOM.render(
+  <CryptoContext>
+    <App />
+  </CryptoContext>,
+  document.getElementById("root")
+);
