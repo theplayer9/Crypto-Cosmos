f9b1287959efcf8cc5153bc7be9225951dd5cdb1
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-18 01:09:57 +0530

    working

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,9 +1,27 @@
-import React, { createContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const Crypto = createContext();
+const Crypto = createContext();  //here we have created the object(Crypto) of the createContext
 
 const CryptoContext = ({ children }) => {
-  return <Crypto.Provider>{children}</Crypto.Provider>;
+    const [currency,setCurrency] = useState("INR")
+    const [symbol, setSymbol] = useState("₹")
+
+
+    useState(()=>{
+        if (currency==="INR") setSymbol("₹")
+        if (currency==="INR") setSymbol('$')
+    },[currency])
+
+
+
+  return <Crypto.Provider value={{currency,symbol,setCurrency}}>{children}</Crypto.Provider>;
 };
 
 export default CryptoContext;
+
+//here we have created a CryptoState, for the current value of the context. we will import this state in the header 
+export const CryptoState= ()=>{
+    return useContext(Crypto)
+    //useContext accepts a context object (the value returned from React.createContext) and returns the current context value for that context. The current context value is determined by the value prop of the nearest <MyContext.Provider> above the calling component in the tree.
+
+}
