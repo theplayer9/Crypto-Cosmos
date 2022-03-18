f0c817f9b9353f4e26fee51a6c5f435164edefd3
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-19 01:08:10 +0530

    added an image to the homepage

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import { makeStyles} from '@material-ui/styles'
+import Container from '@material-ui/core/Container'
+import React from 'react'
+const useStyle = makeStyles(()=>({
+    banner:{
+          backgroundImage:"url(./img2.jpg)"
+    },
+    bannerContent:{
+        height:400,
+        display:"flex",
+        flexDirection:"column",
+        paddingTop:25,
+        justifyContent: "space-around"
+    }
+
+}))
+
+const Banner = () => {
+    const classes = useStyle()
+  return (
+    <div className={classes.banner}>
+        <Container className={classes.bannerContent} >
+
+        </Container>
+    </div>
+  )
+}
+
+export default Banner
