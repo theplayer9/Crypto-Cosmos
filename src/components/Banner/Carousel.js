a9c8b0609e4b4e68b26ba8c77cf2e4a9e9f2301a
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-22 22:39:49 +0530

    Added Axios

--- src/components/Banner/Carousel.js
+++ src/components/Banner/Carousel.js
@@ -1,8 +1,26 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
 
+
+const useStyle = makeStyles(()=>({
+    carousel:{
+        height:"50%",
+        display:"flex",
+        alignItems: "center",
+        // color:"red"
+    },
+
+}))
+
 const Carousel = () => {
+    const classes = useStyle()
+    const fetchTrendingCoins =()=>{
+        
+    }
+  
   return (
-    <div>Carousel</div>
+      
+    <div className={classes.carousel}>Carousel</div>
   )
 }
 
