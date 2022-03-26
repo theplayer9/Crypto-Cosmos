c1551786a5d51d2b150ac99159f94098d0456c87
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-26 12:08:25 +0530

    Set the dark them using Themeprovider.

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,20 +1,38 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { CoinList } from "../config/Api";
+import { CryptoState } from "../CryptoContext";
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchCoins = async () => {
+  const { currency } = CryptoState();
+
+  const fetchCoins = async (cry) => {
     setLoading(true);
-    const { data } = await axios.get(CoinList());
+    const { data } = await axios.get(CoinList(cry));
     setCoins(data);
     setLoading(false);
   };
 
-  return <div>CoinsTable</div>;
+  console.log("coinstable data:", coins);
+  useEffect(() => {
+    fetchCoins(currency);
+  }, [currency]);
+
+  const darkTheme = createTheme(() => ({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  }));
+
+  return <ThemeProvider theme={darkTheme}></ThemeProvider>;
 };
 
 export default CoinsTable;
