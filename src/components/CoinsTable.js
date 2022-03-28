2e1b68b648f383646beed24d40cfe32ade7c65fb
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-29 00:23:18 +0530

    pagination

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -21,6 +21,7 @@ import { CryptoState } from "../CryptoContext";
 import { makeStyles } from "@material-ui/styles";
 import { useHistory } from "react-router-dom";
 import { numberWithCommas } from "./Banner/Carousel";
+import { Pagination } from "@material-ui/lab";
 
 const useStyle = makeStyles(() => ({
   searchbar: {
@@ -45,6 +46,7 @@ const CoinsTable = () => {
   const [search, setSearch] = useState("");
   const history = useHistory();
   const { currency, symbol } = CryptoState();
+  const [page, setPage] = useState(1);
 
   const fetchCoins = async (cry) => {
     setLoading(true);
@@ -119,62 +121,79 @@ const CoinsTable = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {handleSearch().map((row) => {
-                  const profit = row.price_change_percentage_24h > 0;
-                  return (
-                    <TableRow
-                      onClick={() => history.push(`/coins/${row.id}`)}
-                      className={classes.row}
-                      key={row.name}
-                    >
-                      <TableCell
-                        component="th"
-                        scope="row"
-                        style={{ display: "flex", gap: 15, color: "white" }}
+                {handleSearch()
+                  .slice((page - 1) * 10, (page - 1) * 10 + 10)
+                  .map((row) => {
+                    const profit = row.price_change_percentage_24h > 0;
+                    return (
+                      <TableRow
+                        onClick={() => history.push(`/coins/${row.id}`)}
+                        className={classes.row}
+                        key={row.name}
                       >
-                        <img
-                          src={row?.image}
-                          alt={row.name}
-                          height="50"
-                          style={{ marginBottom: 10 }}
-                        />
-                        <div
-                          style={{ display: "flex", flexDirection: "column" }}
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          style={{ display: "flex", gap: 15, color: "white" }}
                         >
-                          <span
-                            style={{ textTransform: "uppercase", fontSize: 22 }}
+                          <img
+                            src={row?.image}
+                            alt={row.name}
+                            height="50"
+                            style={{ marginBottom: 10 }}
+                          />
+                          <div
+                            style={{ display: "flex", flexDirection: "column" }}
                           >
-                            {row.symbol}
-                          </span>
-                          <span style={{ color: "darkgrey" }}>{row.name}</span>
-                        </div>
-                      </TableCell>
-                      <TableCell align="right" style={{ color: "white" }}>
-                        {symbol}
-                        {numberWithCommas(row.current_price.toFixed(2))}
-                      </TableCell>
-                      <TableCell
-                        align="right"
-                        style={{
-                          color: profit > 0 ? "rgb(14,203,129)" : "red",
-                          fontWeight: 500,
-                        }}
-                      >
-                        {profit && "+"}
-                        {row.price_change_percentage_24h.toFixed(2)}%
-                      </TableCell>
-                      <TableCell align="right" style={{ color: "white" }}>
-                        {symbol}
-                        {row.market_cap.toString().slice(0, -6)}M
-                        {/* .slice() is to remove the last 6 digits. Without using it the number commint form the api will be too long to display. */}
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
+                            <span
+                              style={{
+                                textTransform: "uppercase",
+                                fontSize: 22,
+                              }}
+                            >
+                              {row.symbol}
+                            </span>
+                            <span style={{ color: "darkgrey" }}>
+                              {row.name}
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell align="right" style={{ color: "white" }}>
+                          {symbol}
+                          {numberWithCommas(row.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            color: profit > 0 ? "rgb(14,203,129)" : "red",
+                            fontWeight: 500,
+                          }}
+                        >
+                          {profit && "+"}
+                          {row.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell align="right" style={{ color: "white" }}>
+                          {symbol}
+                          {row.market_cap.toString().slice(0, -6)}M
+                          {/* .slice() is to remove the last 6 digits. Without using it the number commint form the api will be too long to display. */}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
               </TableBody>
             </Table>
           )}
         </TableContainer>
+        <Pagination
+          style={{
+            padding: 20,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          count={(handleSearch()?.length / 10).toFixed(0)}
+          color="secondary"
+        />
       </Container>
     </ThemeProvider>
   );
