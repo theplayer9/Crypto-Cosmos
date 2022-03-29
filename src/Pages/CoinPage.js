53c03c56ebd6d05f1bfee08b52727467878d55de
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-29 18:27:56 +0530

    making coins page

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/Api";
+import { CryptoState } from "../CryptoContext";
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles(() => {});
 
 const CoinPage = () => {
-  return (
-    <div>CoinPage</div>
-  )
-}
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+  const { currency, symbol } = CryptoState();
+  const classes = useStyle();
 
-export default CoinPage
+  const fetchCoin = async (naughty) => {
+    const { data } = await axios.get(SingleCoin(naughty));
+    setCoin(data);
+  };
+  console.log("coin data:", coin);
+
+  useEffect(() => {
+    fetchCoin(id);
+  }, [id]);
+
+  return <div>CoinPage</div>;
+};
+
+export default CoinPage;
