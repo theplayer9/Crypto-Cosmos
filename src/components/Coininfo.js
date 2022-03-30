3aec9bfceae624987840303cb2bbc2868c6c6012
Om Sharma <omsharma.theplayer11@gmail.com>
2022-03-30 18:13:36 +0530

    making chart

--- src/components/Coininfo.js
+++ src/components/Coininfo.js
@@ -1,7 +1,75 @@
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import { HistoricalChart } from "../config/Api";
+import { CryptoState } from "../CryptoContext";
 
-const Coininfo = () => {
-  return <div style={{ color: "white" }}>Coininfo</div>;
+const useStyle = makeStyles((theme) => ({
+  container: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: 0,
+      padding: 20,
+      paddingTop: 0,
+    },
+  },
+}));
+
+const Coininfo = ({ coin }) => {
+  const [historicalData, setHistoricalData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+  const classes = useStyle();
+
+  const fetchHistoricalData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricalData(data.price);
+  };
+
+  useEffect(() => {
+    fetchHistoricalData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {!historicalData ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line />
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default Coininfo;
