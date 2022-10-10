31da03473f12956f2a17d8abd3ec6a42130a3031
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-10 20:18:10 +0530

    Initial configuration done!

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjmqrbppDVtzte0Mj0RN_T5-hYo4Myhho",
+  authDomain: "crypto-cosmos-a869e.firebaseapp.com",
+  projectId: "crypto-cosmos-a869e",
+  storageBucket: "crypto-cosmos-a869e.appspot.com",
+  messagingSenderId: "875797069777",
+  appId: "1:875797069777:web:129d32ee59d66356daeaf0",
+};
+
+export default firebaseConfig;
