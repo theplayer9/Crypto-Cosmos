406b310ef55e7d389bf4b018ac8a90819f8e18bd
Om Sharma <omsharma.theplayer11@gmail.com>
2022-10-30 23:14:38 +0530

    login page

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,7 +1,42 @@
-import React from "react";
+import { Box, Button, TextField } from "@material-ui/core";
+import React, { useState } from "react";
 
 const Login = ({ handleClose }) => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {};
+  return (
+    <Box
+      p={3}
+      style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+    >
+      <TextField
+        variant="outlined"
+        type="email"
+        value={email}
+        label="Enter Email"
+        onChange={(e) => setEmail(e.target.value)}
+        fullWidth
+      />
+      <TextField
+        variant="outlined"
+        type="password"
+        value={password}
+        label="Enter Password"
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth
+      />
+      <Button
+        variant="contained"
+        size="large"
+        style={{ background: "#EEBC1D" }}
+        onClick={handleSubmit}
+      >
+        Login
+      </Button>
+    </Box>
+  );
 };
 
 export default Login;
