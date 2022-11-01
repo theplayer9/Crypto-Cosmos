09eb656b3f762acd3f1103dd5b4350ebf287207d
Om Sharma <omsharma.theplayer11@gmail.com>
2022-11-01 23:01:39 +0530

    changes

--- src/components/Authentication/UserSidebar.js
+++ src/components/Authentication/UserSidebar.js
@@ -1,7 +1,53 @@
 import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
 
-const UserSidebar = () => {
-  return <div>UserSidebar</div>;
-};
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: "auto",
+  },
+  sidebar: {
+    // backgroundColor: "grey"
+  },
+});
 
-export default UserSidebar;
+export default function UserSidebar() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  return (
+    <div className={classes.sidebar}>
+      {["right"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          {/* <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button> */}
+
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            hello
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
