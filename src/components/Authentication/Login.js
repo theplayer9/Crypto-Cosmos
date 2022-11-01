5fdf7445ee7da61269b29fccb47538d0976639b2
Om Sharma <omsharma.theplayer11@gmail.com>
2022-11-01 19:01:48 +0530

    login functionality working

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,11 +1,41 @@
 import { Box, Button, TextField } from "@material-ui/core";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
+import CryptoContext, { CryptoState } from "../../CryptoContext";
+import { auth } from "../../firebase";
 
 const Login = ({ handleClose }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { setAlert } = CryptoState();
 
-  const handleSubmit = () => {};
+  const handleSubmit = async () => {
+    if (!email || !password) {
+      setAlert({
+        open: true,
+        message: " Please fill the fields",
+        type: "error",
+      });
+      return;
+    }
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      setAlert({
+        open: true,
+        message: `Login up Successful. Welcome ${result.user.email}`,
+        type: "success",
+      });
+      handleClose();
+    } catch (error) {
+      setAlert({
+        open: true,
+        message: error.message,
+        type: "error",
+      });
+      return;
+    }
+  };
+
   return (
     <Box
       p={3}
